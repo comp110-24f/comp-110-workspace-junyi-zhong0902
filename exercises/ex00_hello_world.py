ecb2f4bebe948af3a0cef673a12a544fd5272c49
Junyi Zhong <junyizh@unc.edu>
2024-08-23 22:05:28 -0400

    Almost Finished Exercise 00 need to submit

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,10 @@
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+"""function call expression"""
+greet(name="Johnny")
+
+"""My first exercise in COMP110"""
+__author__ = "730776350"
